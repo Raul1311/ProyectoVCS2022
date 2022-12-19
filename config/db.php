87c746515dcf57b54bf64835b70e26d4fcf1ed73
Raul1311 <L181061142@mulege.tecnm.mx>
2022-12-18 17:20:06 -0700

    se modificaron los archivos de la base de datos y se agregaron descripciones al archivo catalogo

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 class Conectar{
     public static function conexion(){
-        $conexion= new mysqli("localhost", "root", "", "mvc");
+        $conexion= new mysqli("localhost", "root", "", "mvc.contacto");
         $conexion->query("SET NAMES 'utf8'");
     }
     
